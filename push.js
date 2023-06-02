c805f6b43a790f2103432a9482f77db3d72ac79f
Suhassssss <104508217+Suhassssss@users.noreply.github.com>
2023-06-02 13:47:06 +0530

    fix

--- push.js
+++ push.js
@@ -11,6 +11,15 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:suhasrlawate99@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:
+        "https://fcm.googleapis.com/fcm/send/fLx9crZsLEg:APA91bFcV7kiNUVM_Sai744eHPut-wIGGQLFKNIfDkjL-HNxsH4rDI89xoJ13yskru0FBGuZGlBSEMWCyAWmMsVjYpUwtgcCsyJZIn0OO0vjGAWkNgeIjjvGmvMBO60NafHRX8mqd1DO",
+        expirationTime:null,
+        keys:{
+            p256dh:
+                "BF_p3qReW3FEWREN3XHJGuJaAqA4e9lWUDELztZarRw_x9Nhw8Lv7CGyY1q_VsPiwLBiO93IImdNoxjWZM4AQmg",
+            auth:"tNsTpeg8Z9_qO20EM9Jyfg"
+        }
+    };
 
 push.sendNotification(sub, 'test message')
